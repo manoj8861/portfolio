425239743759643ea2276dfdcfd0d290f6e121ce
Manoj Kumar <manojkumar.g.k@outlook.com>
2023-09-12 20:20:36 +0530

    update: blog post

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -142,10 +142,10 @@ export default function PostLayout({
                 {children}
               </div>
               <div className='pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300'>
-                <Link href={discussUrl(slug)} rel='nofollow'>
+                {/* <Link href={discussUrl(slug)} rel='nofollow'>
                   {'Discuss on Twitter'}
                 </Link>
-                {` • `}
+                {` • `} */}
                 <Link href={editUrl(fileName)}>{'View on GitHub'}</Link>
               </div>
               <Comments frontMatter={frontMatter} />
