6ff0348479a48b1dd4f728d3be0bca7d326a06ee
manojkumar <manojkumar.g.k@outlook.com>
2023-10-08 02:09:05 +0000

    fix: responsive description

--- components/main.tsx
+++ components/main.tsx
@@ -15,31 +15,26 @@ function Main(props: MainProps): React.ReactElement {
   return (
     <div className='fade-in banner flex flex-1 flex-col justify-center px-6 py-10 dark:text-white lg:px-10'>
       <h1 className='text-3xl font-bold dark:text-white lg:text-5xl'>
-      Hey, I am{' '}
-              <span className="text-primary-color-500 dark:text-primary-color-dark-500">Manoj</span>
+        Hey, I am{' '}
+        <span className='text-primary-color-500 dark:text-primary-color-dark-500'>
+          Manoj
+        </span>
       </h1>
-     
 
-      
+      <div className='grid grid-cols-1 gap-6 sm:grid-cols-3'>
+        <div className='col-span-2'>
+          <p className='my-4 text-lg lg:text-2xl'>Welcome to my blog!</p>
+          <br></br>
+          <p className='font-light lg:text-xl'>
+            I'm a Full Stack Engineer who is passionate about automating and
+            building solution for complex problems through code.
+          </p>
+        </div>
+      </div>
 
-      
-      <p className='my-4 text-lg lg:text-2xl'>
-        Welcome to my blog! 
-        </p>
-        <br></br>
-        <p className='font-light lg:text-xl'>
-        I'm a Full Stack Engineer who is passionate about automating and building  
-        <br></br>
-        solution for complex problems through code. 
-      </p>
-
-      
-
-      
       <p className='my-5 font-light lg:text-xl'>
         Feel free to read more
-         
-         <Link className='ml-2 mr-2 font-normal text-black' href='/about'>
+        <Link className='ml-2 mr-2 font-normal text-black' href='/about'>
           <RoughNotation
             show
             type='highlight'
@@ -50,9 +45,7 @@ function Main(props: MainProps): React.ReactElement {
             about me
           </RoughNotation>
         </Link>
-        
-        </p>
-      
+      </p>
     </div>
   );
 }
